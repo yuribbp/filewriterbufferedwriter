5b3d17e7bca1681ed201bddb7007c666cb510a1b
Yuri Baldan <yuribbp@gmail.com>
2020-09-22 17:44:49 -0300

    Acrescentando ao arquivo existente

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,7 +11,7 @@ public class Program {
 		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
 		String path = "c:\\temp\\out.txt";
 		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
 			for (String line : lines) {
 				bw.write(line);
 				bw.newLine();
